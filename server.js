2a10d2cb7f9eaf47a023996b9aa30abd247c7f51
christopher-1989 <64772696+christopher-1989@users.noreply.github.com>
2020-07-26 13:31:36 +1000

    installed node and morgan

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const app = express();
 
+
 const { quotes } = require('./data');
 const { getRandomElement } = require('./utils');
 
 const PORT = process.env.PORT || 4001;
+const morgan = require('morgan')
+app.use(morgan('dev'))
 
 app.use(express.static('public'));
 
+app.listen(PORT, () => console.log(`Web server listening on port ${PORT}`))
