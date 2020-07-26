73ba52e0d9cc1f4ead6fa718b855b300ef308219
christopher-1989 <64772696+christopher-1989@users.noreply.github.com>
2020-07-26 14:12:23 +1000

    using only server file

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const app = express();
-const morgan = require('morgan')
+//const morgan = require('morgan')
 
 const { quotes } = require('./data');
 const { getRandomElement } = require('./utils');
 
 const PORT = process.env.PORT || 4001;
-app.use(morgan('dev'))
+//app.use(morgan('dev'))
 
 app.use(express.static('public'));
+
+app.get('/api/quotes/random', (req, res, next) => {
+    const randomQuote = getRandomElement(quotes)
+    res.send({
+        quote: randomQuote.quote
+    })
+})
 
 app.listen(PORT, () => console.log(`Web server listening on port ${PORT}`))
